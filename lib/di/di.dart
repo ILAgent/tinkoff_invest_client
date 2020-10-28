b0788ff8bb457accf85401c0c3b165e42e61da1c
Ivan Lavrov <ilavrov@yandex-team.ru>
2020-10-28 10:33:24 +0300

    add di

---
+++ lib/di/di.dart
@@ -0,0 +1,16 @@
+import 'package:get_it/get_it.dart';
+import 'package:redux/redux.dart';
+import 'package:tinkoff_api/model/currency.dart';
+import 'package:tinkoff_invest/redux/portfolio_state.dart';
+import 'package:tinkoff_invest/redux/protfolio_state_reducer.dart';
+
+final di = GetIt.instance;
+
+void initDI() {
+  di.registerLazySingleton(() => Store<PortfolioState>(
+        reducePortfolioState,
+        initialState: PortfolioState((b) => b
+          ..currency = Currency.rUB
+          ..amount = 123456.78),
+      ));
+}
