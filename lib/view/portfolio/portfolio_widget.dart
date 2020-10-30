198762b9b2f9eb31eb59a18725d7edcef78b3d9d
Ivan Lavrov <ilavrov@yandex-team.ru>
2020-10-30 22:55:34 +0300

    change currency by tap

--- lib/view/portfolio/portfolio_widget.dart
+++ lib/view/portfolio/portfolio_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:redux/redux.dart';
+import 'package:tinkoff_invest/redux/actions.dart';
 import 'package:tinkoff_invest/redux/portfolio_state.dart';
 import 'package:tinkoff_invest/redux/store_extension.dart';
 import 'package:tinkoff_invest/view/portfolio/children/tinkoff_account_amount.dart';
@@ -13,20 +14,25 @@ class PortfolioWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<PortfolioState>(
-        stream: _store.states,
-        builder: (context, snapshot) {
-          return Container(
-            padding: EdgeInsets.only(top: 50, left: 40, right: 40),
-            color: Colors.white,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TinkoffAccountTileWidget(snapshot.data),
-                TinkoffAccountAmountWidget(snapshot.data),
-              ],
-            ),
-          );
-        });
+    return GestureDetector(
+      onTap: () {
+        _store.dispatch(TogglePortfolioCurrency());
+      },
+      child: StreamBuilder<PortfolioState>(
+          stream: _store.states,
+          builder: (context, snapshot) {
+            return Container(
+              padding: EdgeInsets.only(top: 50, left: 40, right: 40),
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TinkoffAccountTileWidget(snapshot.data),
+                  TinkoffAccountAmountWidget(snapshot.data),
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
