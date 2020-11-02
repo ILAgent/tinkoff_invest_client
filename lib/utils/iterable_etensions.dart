fc82b4ee23fc836926c0ec22ad53f981874d7fa5
Ivan Lavrov <ilavrov@yandex-team.ru>
2020-11-02 16:04:06 +0300

    fix declaration

--- lib/utils/iterable_etensions.dart
+++ lib/utils/iterable_etensions.dart
@@ -1,5 +1,5 @@
 extension ListExtensions<T> on Iterable<T> {
-  Future<double> sumAsync(Future<double> extract(T)) async {
+  Future<double> sumAsync(Future<double> extract(T item)) async {
     double result = 0.0;
     for (T element in this) {
       result += await extract(element);
