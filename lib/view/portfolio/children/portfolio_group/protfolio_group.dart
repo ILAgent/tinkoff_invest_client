4b5509a9a8c9cd0c66c733f221fbed2d11032f77
Ivan Lavrov <ilavrov@yandex-team.ru>
2020-11-12 22:20:00 +0300

    add group

---
+++ lib/view/portfolio/children/portfolio_group/protfolio_group.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:tinkoff_invest/redux/state/items_group.dart';
+
+class PortfolioGroupWidget extends StatelessWidget {
+  final ItemsGroup _group;
+
+  PortfolioGroupWidget(this._group) : super(key: ValueKey(_group));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      alignment: Alignment.center,
+      child: Text(_group.title),
+    );
+  }
+}
--- lib/view/portfolio/children/portfolio_items_list.dart
+++ lib/view/portfolio/children/portfolio_items_list.dart
@@ -1,9 +1,13 @@
+import "package:collection/collection.dart";
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tinkoff_invest/redux/portfolio_store.dart';
+import 'package:tinkoff_invest/redux/state/items_group.dart';
 import 'package:tinkoff_invest/redux/state/portfolio_item.dart';
+import 'package:tinkoff_invest/redux/state/portfolio_state.dart';
 import 'package:tinkoff_invest/redux/store_extension.dart';
+import 'package:tinkoff_invest/view/portfolio/children/portfolio_group/protfolio_group.dart';
 import 'package:tinkoff_invest/view/portfolio/children/portfolio_item/portfolio_item.dart';
 
 class PortfolioItemsList extends StatelessWidget {
@@ -13,15 +17,29 @@ class PortfolioItemsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<PortfolioItem>>(
-      stream: _store.states.map((it) => it.items).distinct(listEquals),
+    return StreamBuilder<List<dynamic>>(
+      stream: _store.states.map(_stateToList).distinct(listEquals),
       builder: (context, snapshot) {
         return ReorderableListView(
-          children: snapshot.data.map((e) => PortfolioItemWidget(e)).toList(),
+          children: snapshot.data.map((e) {
+            if (e is PortfolioItem) return PortfolioItemWidget(e);
+            if (e is ItemsGroup) return PortfolioGroupWidget(e);
+            throw ArgumentError(e);
+          }).toList(),
           padding: EdgeInsets.only(top: 16),
-          onReorder: (int oldIndex, int newIndex) {  },
+          onReorder: (int oldIndex, int newIndex) {},
         );
       },
     );
   }
+}
+
+List<dynamic> _stateToList(PortfolioState state) {
+  return groupBy(state.items, (PortfolioItem item) => item.group)
+      .entries
+      .expand((e) => [
+            if (e.key != null) e.key,
+            ...e.value,
+          ])
+      .toList();
 }
