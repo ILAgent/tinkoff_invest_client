9d66bac1401c06b1817cc91d62419efc867107de
Ivan Lavrov <ilavrov@yandex-team.ru>
2020-11-15 15:31:23 +0300

    remove default group

--- lib/redux/portfolio_store.dart
+++ lib/redux/portfolio_store.dart
@@ -4,9 +4,7 @@ import 'package:tinkoff_api/model/currency.dart';
 import 'package:tinkoff_api/model/money_amount.dart';
 import 'package:tinkoff_invest/redux/dispatcher.dart';
 import 'package:tinkoff_invest/redux/protfolio_state_reducer.dart';
-import 'package:tinkoff_invest/redux/state/items_group.dart';
 import 'package:tinkoff_invest/redux/state/portfolio_state.dart';
-import 'package:uuid/uuid.dart';
 
 import 'epics/portfolio_item_epic.dart';
 import 'epics/total_money_epic.dart';
@@ -16,11 +14,13 @@ class PortfolioStore extends Store<PortfolioState> implements Dispatcher {
       TotalAmountEpic totalAmountEpic, PortfolioItemsEpic portfolioItemsEpic)
       : super(reducePortfolioState,
             initialState: PortfolioState(
-              amount: MoneyAmount((b) => b
-                ..currency = Currency.rUB
-                ..value = 0),
+              amount: MoneyAmount(
+                (b) => b
+                  ..currency = Currency.rUB
+                  ..value = 0,
+              ),
               items: [],
-              groups: [ItemsGroup(title: 'Всё моё', id: Uuid().v1())],
+              groups: [],
             ),
             middleware: [
               EpicMiddleware(totalAmountEpic.act),
