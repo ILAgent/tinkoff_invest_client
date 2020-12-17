e49a6c42c12342f8bf5eba61382110912e4b0a0b
Ivan Lavrov <ilavrov@yandex-team.ru>
2020-12-17 23:59:25 +0300

    disable api generator

--- lib/openapi_generator.dart
+++ lib/openapi_generator.dart
@@ -1,9 +1,9 @@
-import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
-
-@Openapi(
-    additionalProperties:
-        AdditionalProperties(pubName: 'tinkoff_invest_api', pubAuthor: 'IL_Agent'),
-    inputSpecFile: 'swagger.yaml',
-    generatorName: Generator.DART_DIO,
-    outputDirectory: 'tinkoff_invest_api')
-class GeneratorConfig extends OpenapiGeneratorConfig {}
+// import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+//
+// @Openapi(
+//     additionalProperties:
+//         AdditionalProperties(pubName: 'tinkoff_invest_api', pubAuthor: 'IL_Agent'),
+//     inputSpecFile: 'swagger.yaml',
+//     generatorName: Generator.DART_DIO,
+//     outputDirectory: 'tinkoff_invest_api')
+// class GeneratorConfig extends OpenapiGeneratorConfig {}
