67132862b3df3da6588be216f0671a093f96c125
Ivan Lavrov <ilavrov@yandex-team.ru>
2021-04-04 16:54:54 +0300

    fix grouping

--- lib/redux/epics/calculate_groups_epic.dart
+++ lib/redux/epics/calculate_groups_epic.dart
@@ -1,5 +1,4 @@
 import 'package:built_collection/built_collection.dart';
-import 'package:collection/collection.dart';
 import 'package:redux_epics/redux_epics.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/transformers.dart';
@@ -35,34 +34,37 @@ class CalculateGroupsEpic {
       (values) => values[0] as BuiltList<PortfolioItem>,
     );
 
-    return stream //
-        .switchMap<dynamic>((items) {
-      final List<MapEntry<String, List<PortfolioItem>>> groups = groupBy(
-        items,
-        (PortfolioItem item) => item.groupId,
-      ) //
-          .entries
-          .where((e) => e.key != null)
-          .map((e) => MapEntry(e.key!, e.value))
-          .toList();
+    return stream.switchMap<dynamic>((BuiltList<PortfolioItem> items) {
+
+      final List<MapEntry<String, List<PortfolioItem>>> groups = store //
+          .state
+          .groups
+          .map((group) {
+        final itemsInGroup = items.where((item) => item.groupId == group.id).toList();
+        return MapEntry(group.id, itemsInGroup);
+      }).toList();
 
       final actions = Stream.fromIterable(groups).asyncMap((entry) async {
         final targetCurrency = store.state.amount.currency;
-        final convertedAmount = await entry.value.sumAsync((portfolioItem) async {
-          final amount = (portfolioItem.actualPrice ?? 0.0) * portfolioItem.portfolioPosition.balance;
-          final moneyAmount = MoneyAmount((b) => b
-            ..currency = portfolioItem.currency()
-            ..value = amount);
-          return (await _converter.convert(moneyAmount, targetCurrency)).value;
-        });
+        final convertedAmount = entry.value.isEmpty
+            ? null
+            : await entry.value.sumAsync((portfolioItem) async {
+                final amount = (portfolioItem.actualPrice ?? 0.0) * portfolioItem.portfolioPosition.balance;
+                final moneyAmount = MoneyAmount((b) => b
+                  ..currency = portfolioItem.currency()
+                  ..value = amount);
+                return (await _converter.convert(moneyAmount, targetCurrency)).value;
+              });
 
-        final convertedIncome = await entry.value.sumAsync((portfolioItem) async {
-          final income = portfolioItem.income ?? 0.0;
-          final moneyAmountIncome = MoneyAmount((b) => b
-            ..currency = portfolioItem.currency()
-            ..value = income);
-          return (await _converter.convert(moneyAmountIncome, targetCurrency)).value;
-        });
+        final convertedIncome = entry.value.isEmpty
+            ? null
+            : await entry.value.sumAsync((portfolioItem) async {
+                final income = portfolioItem.income ?? 0.0;
+                final moneyAmountIncome = MoneyAmount((b) => b
+                  ..currency = portfolioItem.currency()
+                  ..value = income);
+                return (await _converter.convert(moneyAmountIncome, targetCurrency)).value;
+              });
 
         return UpdateGroupAmounts(amount: convertedAmount, income: convertedIncome, id: entry.key);
       });
